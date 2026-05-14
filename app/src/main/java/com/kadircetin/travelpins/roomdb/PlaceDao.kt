2a5435ed27135a3e6982df1a62731e97323d52b5
Kadir Çetin <kadir.cetin72@gmail.com>
2026-05-14 14:58:12 +0300

    PlaceDao interface is added

---
+++ app/src/main/java/com/kadircetin/travelpins/roomdb/PlaceDao.kt
@@ -0,0 +1,21 @@
+package com.kadircetin.travelpins.roomdb
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.kadircetin.travelpins.model.Place
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Flowable
+
+@Dao
+interface PlaceDao {
+    @Query(value = "SELECT * FROM Place")
+    fun getAll(): Flowable<List<Place>>
+
+    @Insert
+    fun insert(place: Place): Completable
+
+    @Delete
+    fun delete(place: Place): Completable
+}
