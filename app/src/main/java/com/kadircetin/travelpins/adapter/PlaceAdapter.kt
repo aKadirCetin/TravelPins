cb65d1b098843667f6d9ed8934ba1ba26fd327c4
Kadir Çetin <kadir.cetin72@gmail.com>
2026-05-14 15:12:54 +0300

    PlaceAdapter is added

---
+++ app/src/main/java/com/kadircetin/travelpins/adapter/PlaceAdapter.kt
@@ -0,0 +1,36 @@
+package com.kadircetin.travelpins.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.kadircetin.travelpins.databinding.RecyclerRowBinding
+import com.kadircetin.travelpins.model.Place
+import com.kadircetin.travelpins.view.MainActivity
+
+class PlaceAdapter(var placeList: List<Place>) : RecyclerView.Adapter<PlaceAdapter.PlaceHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceHolder {
+        val recyclerRowBinding: RecyclerRowBinding =
+            RecyclerRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PlaceHolder(recyclerRowBinding)
+    }
+
+    override fun onBindViewHolder(holder: PlaceHolder, position: Int) {
+        holder.recyclerRowBinding.recyclerViewTextView.setText(placeList[position].name)
+
+        holder.itemView.setOnClickListener {
+            val intentToMapsActivity = Intent(holder.itemView.context, MainActivity::class.java)
+            intentToMapsActivity.putExtra("selectedPlace",placeList.get(position))
+            intentToMapsActivity.putExtra("info","old")
+            holder.itemView.context.startActivity(intentToMapsActivity)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return placeList.size
+    }
+
+
+    class PlaceHolder(val recyclerRowBinding: RecyclerRowBinding) :
+        RecyclerView.ViewHolder(recyclerRowBinding.root)
+}
