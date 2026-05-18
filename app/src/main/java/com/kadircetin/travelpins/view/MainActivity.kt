cf639d6bbf53a8b69517fab4f546d4d52843c63f
Kadir Çetin <kadir.cetin72@gmail.com>
2026-05-18 11:18:28 +0300

    MapActivity is initialized and permissions added to manifest

--- app/src/main/java/com/kadircetin/travelpins/view/MainActivity.kt
+++ app/src/main/java/com/kadircetin/travelpins/view/MainActivity.kt
@@ -1,20 +1,69 @@
 package com.kadircetin.travelpins.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
+import com.google.android.material.appbar.MaterialToolbar
 import com.kadircetin.travelpins.R
+import com.kadircetin.travelpins.adapter.PlaceAdapter
+import com.kadircetin.travelpins.databinding.ActivityMainBinding
+import com.kadircetin.travelpins.model.Place
+import com.kadircetin.travelpins.roomdb.PlaceDao
+import com.kadircetin.travelpins.roomdb.PlaceDatabase
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val compositeDisposable = CompositeDisposable()
+    private lateinit var placeDao: PlaceDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         settings()
+        setSupportActionBar(binding.toolbar)
+
+        val db = Room.databaseBuilder(applicationContext, PlaceDatabase::class.java, "Places").build()
+        placeDao = db.placeDao()
+
+        compositeDisposable.add(
+            placeDao.getAll()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::handleResponse)
+        )
     }
 
+    private fun handleResponse(placeList: List<Place>) {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = PlaceAdapter(placeList)
+        binding.recyclerView.adapter = adapter
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.place_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.add_place){
+            startActivity(Intent(this, MainActivity::class.java).putExtra("info", "new"))
+        }
+        return super.onOptionsItemSelected(item)
+    }
     private fun settings() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
